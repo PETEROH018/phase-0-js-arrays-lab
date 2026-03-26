adfc5d949a99c7a8df51692d583ec60911b97990
PETEROH018 <mathengep63@gmail.com>
2026-03-27 00:15:56 +0300

    initial commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,29 @@
 // Write your code here
+// 1. Create the Product Inventory Array
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+// 2. Access Product Information
+function logFirstProduct() {
+    console.log(products[0]);
+}
+
+// 3. Update Product Information
+function updateProductName(index, newName) {
+    if (index >= 0 && index < products.length) {
+        products[index] = newName;
+    } else {
+        console.log("Invalid index");
+    }
+}
+
+// 4. Remove a Product
+function removeLastProduct() {
+    products.pop();
+}
+// 5. Add a New Product
+function addProduct() {
+    products.push("Tablet");
+}
 
 
 // Export the necessary parts for testing
@@ -10,3 +34,4 @@ module.exports = {
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
   products
 };
+
